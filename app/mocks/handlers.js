d919bb98178641cb50e251d7b2d62b1d18a74c25
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-04-12 21:54:59 +0100

    update msw

--- app/mocks/handlers.js
+++ app/mocks/handlers.js
@@ -1,9 +1,9 @@
 import {rest} from 'msw'
 
-const baseURL = 'https://easy-shop-be.herokuapp.com/api'
+const baseURL = 'http://10.0.2.2:9090/api'
 
 export const handlers = [
 	rest.post(`${baseURL}/login`, (req, res, ctx) => {
-		return res(ctx.status(200), ctx.json({user: 'ant'}))
+		return res(ctx.status(200), ctx.json({user: 'ant'}), ctx.set('set-cookie', 'jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjliMWRlYjRkLTNiN2QtNGJhZC05YmRkLTJiMGQ3YjNkY2I2ZCIsImVtYWlsIjoiYW50aG9ueW1jZ3JlYWxAaG90bWFpbC5jby51ayIsImlhdCI6MTY4MTMzMjc2OSwiZXhwIjoxNjgxMzQzNTY5fQ.m4vkHrlsu8uRPHoTzixRxE137-kv5htI-n_HySml9tA; Path=/'))
 	})
 ]
