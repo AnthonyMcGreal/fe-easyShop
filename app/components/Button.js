5e2d9a86e8d24d7377bf71942cec96b6a901ff1a
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-05-15 20:28:41 +0100

    update button component

--- app/components/Button.js
+++ app/components/Button.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Text from './Text'
 import {StyleSheet, Pressable} from 'react-native'
 
-const Button = ({onPress, buttonText}) => {
+const Button = ({onPress, buttonText, disabled}) => {
 	return (
 		<Pressable
 			accessibilityRole="button"
 			accessibilityLabel={`${buttonText}`}
-			style={styles.button}
+			style={disabled? [styles.button, styles.disabled]: styles.button}
 			onPress={onPress}
+			disabled={disabled}
 		>
 			<Text style={styles.text}>{`${buttonText}`}</Text>
 		</Pressable>
@@ -27,7 +28,13 @@ const styles = StyleSheet.create({
 	text: {
 		color: 'white',
 		fontSize: 16
-	}
+	},
+	disabled: {
+		backgroundColor: '#F1F2F6',
+		borderColor: '#642CA9',
+		borderWidth: 3,
+		color: '#B5B5B5'
+	},
 })
 
 export default Button
