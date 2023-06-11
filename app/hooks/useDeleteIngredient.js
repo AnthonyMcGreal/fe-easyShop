80fa792302146cd75ac062a2761a3ac5afa9ddae
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-06-11 18:58:48 +0100

    add delete ingredient hook

---
+++ app/hooks/useDeleteIngredient.js
@@ -0,0 +1,28 @@
+import {useState} from 'react'
+import {deleteIngredientById} from '../api'
+import {useAuthContext} from '../components/AuthContext'
+
+const useDeleteIngredient = () => {
+	const token = useAuthContext()
+
+	const [hasError, setHasError] = useState(false)
+	const [isLoading, setIsLoading] = useState(false)
+	const [isSuccess, setIsSuccess] = useState(false)
+
+	const deleteIngredient = async ingredientID => {
+		console.log(ingredient)
+		setIsLoading(true)
+		const apiResponse = await deleteIngredientById(ingredientID, token)
+		if (apiResponse.status === 204) {
+			setIsSuccess(true)
+			setHasError(false)
+		} else {
+			setIsSuccess(false)
+			setHasError(true)
+		}
+		setIsLoading(false)
+	}
+	return {hasError, isLoading, isSuccess, deleteIngredient}
+}
+
+export default useDeleteIngredient
