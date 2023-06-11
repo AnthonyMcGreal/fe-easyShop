1e7ad3e771f8f746cd6c372506b1b05ce5ead8ea
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-06-11 18:38:53 +0100

    update deleteIngredientWIP

---
+++ app/hooks/useGetIngredients.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+import {getIngredients as getIngredientsApiCall} from '../api'
+import {useUserContext} from '../components/UserContext'
+import {useAuthContext} from '../components/AuthContext'
+
+const useGetIngredients = () => {
+	const user = useUserContext()
+	const token = useAuthContext()
+
+	const [hasError, setHasError] = useState(false)
+	const [isLoading, setIsLoading] = useState(true)
+	const [ingredients, setIngredients] = useState([])
+
+	const getIngredients = async () => {
+		setHasError(false)
+		setIsLoading(true)
+		const ingredients = await getIngredientsApiCall(user.user_id, token)
+		if (items.status === 200) {
+			setIngredients(ingredients.data.ingredients)
+			setHasError(false)
+		} else {
+			setIngredients([])
+			setHasError(true)
+		}
+		setIsLoading(false)
+	}
+
+	return {hasError, isLoading, ingredients, getIngredients}
+}
+
+export default useGetIngredients
--- app/screens/home/recipes/DeleteIngredient.js
+++ app/screens/home/recipes/DeleteIngredient.js
@@ -1,206 +1,46 @@
 import {useEffect, useState} from 'react'
-import {
-	Text,
-	StyleSheet,
-	Pressable,
-	Modal,
-	View,
-	ActivityIndicator
-} from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context'
-import SelectDropdown from 'react-native-select-dropdown'
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
-import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
-import {getIngredients, deleteIngredientById} from '../../../api'
-import {useUserContext} from '../../../components/UserContext'
-import {useAuthContext} from '../../../components/AuthContext'
+import Button from '../../../components/Button'
+import Spacer from '../../../components/Spacer'
+import DropDownList from '../../../components/DropDownList'
+import useDeleteMiscItems from '../../../hooks/useDeleteMiscItem'
+import PageLoading from '../../../components/PageLoading'
+import DeleteMiscItemModal from '../../../Models/DeleteMiscItemModal'
+import ApiFallback from '../../../Models/ApiFallback'
+import ScreenBase from '../../../components/ScreenBase'
+import Text from '../../../components/Text'
+import useGetIngredients from '../../../hooks/useGetIngredients'
 
-const DeleteMiscItem = ({navigation}) => {
-	const user = useUserContext()
-	const token = useAuthContext()
+const DeleteIngredient = () => {
+	const [ingredientToBeDeleted, setIngredientToBeDeleted] = useState('')
+	const [isModalOpen, setIsModalOpen] = useState(false)
+	const {hasError, isLoading, ingredients, getIngredients} = useGetIngredients()
+	const ingredientNames = ingredients? ingredients.map(ingredient => ingredient.name) : []
+		// useDeleteIngredient
 
-	const [ingredients, setIngredients] = useState([])
-	const [ingredientNames, setIngredientNames] = useState([])
-	const [ingredientToDelete, setIngredientToDelete] = useState('')
-	const [ingredientIdToDeleted, setIngredientIdToDeleted] = useState()
-	const [modalVisible, setModalVisible] = useState(true)
-	const [apiResult, setApiResult] = useState(0)
-	const [getIngredientsBeenCalled, setGetIngredientsBeenCalled] =
-		useState(false)
+		useEffect(() => {
+			//getIngredients()
+		}, [])
 
-	useEffect(() => {
-		const asyncGetIngredients = async () => {
-			const items = await getIngredients(user.user_id, token)
-			setIngredients(items.data.ingredients)
-			setApiResult(items.status)
-			setGetIngredientsBeenCalled(true)
-			setIngredientNames(ingredients.map(ingredient => ingredient.name))
-			setModalVisible(false)
-		}
-		asyncGetIngredients()
-	}, [getIngredientsBeenCalled])
+	const handleDeleteIngredient = () => {}
 
-	useEffect(() => {
-		const ingredientId = ingredients
-			.filter(ingredient => ingredient.name === ingredientToDelete)
-			.map(ingredient => {
-				return ingredient.ingredient_id
-			})
-		setIngredientIdToDeleted(ingredientId[0])
-	}, [ingredientToDelete])
+	const onDropDwonSelect = ingredientName => {
+    const selectedIngredient = ingredients.filter(ingredient => ingredient.name === ingredientName)
+    setIngredientToBeDeleted(selectedIngredient[0])
+  }
 
-	const handleDeleteIngredientButton = async () => {
-		setModalVisible(true)
-		let result = await deleteIngredientById(ingredientIdToDeleted, token)
-		setApiResult(result.status)
-	}
-
-	const backButton = () => {
-		return (
-			<Pressable
-				style={styles.afterActionButton}
-				onPress={() => {
-					navigation.navigate('RecipesHome')
-				}}
-			>
-				<Text style={styles.text}>Back to Recipes</Text>
-			</Pressable>
-		)
-	}
+	//apiFallback
+	//isLoading
+	//Modal
 
 	return (
-		<SafeAreaView style={styles.background}>
-			<Modal
-				animationType="fade"
-				visible={modalVisible}
-				onRequestClose={() => {
-					Alert.alert('Modal has been closed.')
-					setModalVisible(modalVisible)
-				}}
-			>
-				<View style={styles.background}>
-					<View style={styles.modelContainer}>
-						{apiResult === 204 ? (
-							<View>
-								<Text style={styles.afterActionText}>Ingredient Deleted</Text>
-								{backButton()}
-							</View>
-						) : null}
-						{apiResult > 0 && apiResult !== 204 ? (
-							<View>
-								<Text style={styles.afterActionText}>
-									Ooops! Something went wrong, try again
-								</Text>
-								{backButton()}
-							</View>
-						) : null}
-						{apiResult === 0 ? (
-							<ActivityIndicator
-								size="large"
-								color="#6D2D55"
-								animating={true}
-							/>
-						) : null}
-					</View>
-				</View>
-			</Modal>
-			<View style={styles.contentContainer}>
-				<Text style={styles.text}>Pick an ingredient to delete</Text>
-				<SelectDropdown
-					data={ingredientNames}
-					onSelect={(selectedIngredient, index) => {
-						setIngredientToDelete(selectedIngredient)
-					}}
-					buttonTextAfterSelection={(selectedIngredient, index) => {
-						return `${selectedIngredient}`
-					}}
-					renderDropdownIcon={() => {
-						return <FontAwesomeIcon icon={faChevronDown} />
-					}}
-					buttonStyle={styles.dropDownStyle}
-					buttonTextStyle={styles.dropDownText}
-					rowStyle={styles.rowStyle}
-				/>
-			</View>
-			<Pressable
-				style={styles.button}
-				disabled={!ingredientToDelete}
-				onPress={() => handleDeleteIngredientButton()}
-			>
-				<Text style={styles.text}>Delete Ingredient</Text>
-			</Pressable>
-		</SafeAreaView>
+		<ScreenBase>
+			<Spacer spaceRequired={40} />
+			<Text>Pick an ingredient to delete</Text>
+      <DropDownList listData={ingredientNames} onSelect={onDropDwonSelect} />
+      <Spacer spaceRequired={18} />
+      <Button onPress={handleDeleteIngredient} buttonText='Delete ingredient' disabled={!ingredientToBeDeleted} />
+		</ScreenBase>
 	)
 }
 
-const styles = StyleSheet.create({
-	background: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#2d556d',
-		width: '100%'
-	},
-	contentContainer: {
-		height: 200,
-		width: '80%',
-		alignItems: 'center'
-	},
-	text: {
-		color: 'white',
-		fontSize: 18,
-		fontFamily: 'Nunito'
-	},
-	dropDownStyle: {
-		width: 250,
-		height: 50,
-		margin: 12,
-		borderWidth: 1,
-		backgroundColor: 'lightgrey',
-		marginBottom: 60
-	},
-	dropDownText: {
-		lineHeight: 20,
-		fontSize: 16,
-		paddingLeft: 10,
-		fontFamily: 'Nunito'
-	},
-	rowStyle: {
-		backgroundColor: 'lightgrey'
-	},
-	modelContainer: {
-		backgroundColor: '#2d556d',
-		width: '80%',
-		height: '80%'
-	},
-	button: {
-		backgroundColor: '#6D2D55',
-		width: 250,
-		height: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 10
-	},
-	afterActionText: {
-		color: '#6D2D55',
-		fontSize: 30,
-		textAlign: 'center',
-		marginBottom: 70,
-		marginTop: 120,
-		fontFamily: 'Nunito',
-		textShadowColor: 'white',
-		textShadowRadius: 12
-	},
-	afterActionButton: {
-		backgroundColor: '#6D2D55',
-		width: '70%',
-		height: 50,
-		marginLeft: '15%',
-		marginTop: 60,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 10
-	}
-})
-
-export default DeleteMiscItem
+export default DeleteIngredient
