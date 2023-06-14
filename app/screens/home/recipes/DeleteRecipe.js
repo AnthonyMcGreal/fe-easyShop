95f5f956aa18e2ecba30b7825507f60133dec0d8
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-06-14 22:08:39 +0100

    update delete recipe screen

--- app/screens/home/recipes/DeleteRecipe.js
+++ app/screens/home/recipes/DeleteRecipe.js
@@ -1,197 +1,63 @@
 import {useEffect, useState} from 'react'
-import {
-	Text,
-	StyleSheet,
-	Pressable,
-	Modal,
-	View,
-	ActivityIndicator
-} from 'react-native'
-import {SafeAreaView} from 'react-native-safe-area-context'
-import SelectDropdown from 'react-native-select-dropdown'
-import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome'
-import {faChevronDown} from '@fortawesome/free-solid-svg-icons'
-import {getRecipes, deleteRecipeByName} from '../../../api'
-import {useUserContext} from '../../../components/UserContext'
-import {useAuthContext} from '../../../components/AuthContext'
+import Button from '../../../components/Button'
+import Spacer from '../../../components/Spacer'
+import DropDownList from '../../../components/DropDownList'
+import PageLoading from '../../../components/PageLoading'
+import DeleteRecipeModal from '../../../Models/DeleteRecipeModal'
+import ApiFallback from '../../../Models/ApiFallback'
+import ScreenBase from '../../../components/ScreenBase'
+import Text from '../../../components/Text'
+import useDeleteRecipe from '../../../hooks/useDeleteRecipe'
+import useGetRecipes from '../../../hooks/useGetRecipes'
 
-function DeleteRecipe({navigation}) {
-	const user = useUserContext()
-	const token = useAuthContext()
+const DeleteRecipe = () => {
+  const [recipeToBeDeleted, setRecipeToBeDeleted] = useState('')
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const {hasError, isLoading, recipes, getRecipes} = useGetRecipes()
+  const recipeNames = recipes? recipes.map(recipe => recipe.recipe_name) : []
+  const {hasError : hasDeleteError, isLoading: isDeleteLoading, isSuccess, deleteRecipe} = useDeleteRecipe()
 
-	const [recipes, setRecipes] = useState([])
-	const [recipeNames, setRecipeNames] = useState([])
-	const [recipeToDelete, setRecipeToDelete] = useState(0)
-	const [modalVisible, setModalVisible] = useState(true)
-	const [apiResult, setApiResult] = useState(0)
-	const [getRecipesHasBeenCalled, setGetRecipesHasBeenCalled] = useState(false)
+  useEffect(() => {
+    getRecipes()
+  }, [])
 
-	useEffect(() => {
-		const asyncGetRecipes = async () => {
-			const items = await getRecipes(user.user_id, token)
-			setRecipes(items.data.recipes)
-			setApiResult(items.status)
-			setGetRecipesHasBeenCalled(true)
-			setRecipeNames(recipes.map(recipe => recipe.recipe_name))
-			setModalVisible(false)
-		}
-		asyncGetRecipes()
-	}, [getRecipesHasBeenCalled])
+  const handleDeleteRecipe = () => {
+    setIsModalOpen(true)
+    deleteRecipe(recipeToBeDeleted)
+  }
 
-	const handleDeleteRecipeButton = async () => {
-		setModalVisible(true)
-		let result = await deleteRecipeByName(recipeToDelete, user.user_id, token)
-		setApiResult(result.status)
-	}
+  if (hasError || hasDeleteError)
+		return <ApiFallback goBackScreen={'RecipeHome'} buttonText={'Back to Recipes'} />
+    if (isLoading) return <PageLoading />
+  if(isModalOpen)
+    return (
+      <DeleteRecipeModal 
+      isDeleteLoading={isDeleteLoading}
+      isSuccess={isSuccess}
+      isModalOpen={isModalOpen}
+      setIsModalOpen={setIsModalOpen}
+      />
+    )
 
-	const backButton = () => {
-		return (
-			<Pressable
-				style={styles.modalButton}
+  const onRecipeDropdownSelect = recipeName => {
+    setRecipeToBeDeleted(recipeName)
+  }
+
+  return (
+    <ScreenBase>
+      <Spacer spaceRequired={40} />
+      <Text>Pick a recipe to delete</Text>
+      <DropDownList listData={recipeNames} onSelect={onRecipeDropdownSelect} />
+      <Spacer spaceRequired={18} />
+			<Button
 				onPress={() => {
-					navigation.navigate('RecipesHome')
+					handleDeleteRecipe()
 				}}
-			>
-				<Text style={styles.text}>Back to Recipes</Text>
-			</Pressable>
-		)
-	}
-
-	return (
-		<SafeAreaView style={styles.background}>
-			<Modal
-				animationType="fade"
-				visible={modalVisible}
-				onRequestClose={() => {
-					setModalVisible(modalVisible)
-				}}
-			>
-				<View style={styles.background}>
-					<View style={styles.modelContainer}>
-						{apiResult === 204 ? (
-							<View>
-								<Text style={styles.afterActionText}>Recipe Deleted</Text>
-								{backButton()}
-							</View>
-						) : null}
-						{apiResult > 0 && apiResult !== 204 ? (
-							<View>
-								<Text style={styles.afterActionText}>
-									Ooops! Something went wrong, try again
-								</Text>
-								{backButton()}
-							</View>
-						) : null}
-						{apiResult === 0 ? (
-							<ActivityIndicator
-								size="large"
-								color="#6D2D55"
-								animating={true}
-							/>
-						) : null}
-					</View>
-				</View>
-			</Modal>
-			<View style={styles.contentContainer}>
-				<Text style={styles.text}>Pick a recipe to delete</Text>
-				<SelectDropdown
-					data={recipeNames}
-					onSelect={selectedRecipe => {
-						setRecipeToDelete(selectedRecipe)
-					}}
-					buttonTextAfterSelection={selectedRecipe => {
-						return `${selectedRecipe}`
-					}}
-					renderDropdownIcon={() => {
-						return <FontAwesomeIcon icon={faChevronDown} />
-					}}
-					buttonStyle={styles.dropDownStyle}
-					buttonTextStyle={styles.dropDownText}
-					rowStyle={styles.rowStyle}
-				/>
-			</View>
-			<Pressable
-				style={styles.button}
-				disabled={!recipeToDelete}
-				onPress={() => handleDeleteRecipeButton()}
-			>
-				<Text style={styles.text}>Delete recipe</Text>
-			</Pressable>
-		</SafeAreaView>
-	)
+				buttonText="Delete recipe"
+				disabled={!recipeToBeDeleted}
+			/>
+    </ScreenBase>
+  )
 }
-
-const styles = StyleSheet.create({
-	background: {
-		flex: 1,
-		alignItems: 'center',
-		backgroundColor: '#2d556d',
-		width: '100%'
-	},
-	contentContainer: {
-		height: 120,
-		width: '80%',
-		alignItems: 'center',
-		marginTop: 150
-	},
-	text: {
-		color: 'white',
-		fontSize: 18,
-		fontFamily: 'Nunito'
-	},
-	dropDownStyle: {
-		width: 250,
-		height: 50,
-		margin: 12,
-		borderWidth: 1,
-		backgroundColor: 'lightgrey',
-		marginBottom: 60
-	},
-	dropDownText: {
-		lineHeight: 20,
-		fontSize: 16,
-		paddingLeft: 10,
-		fontFamily: 'Nunito'
-	},
-	rowStyle: {
-		backgroundColor: 'lightgrey'
-	},
-	modelContainer: {
-		backgroundColor: '#2d556d',
-		width: '80%',
-		height: 500,
-		display: 'flex',
-		flexDirection: 'column',
-		justifyContent: 'space-around',
-		alignItems: 'center'
-	},
-	afterActionText: {
-		color: '#6D2D55',
-		fontSize: 30,
-		textAlign: 'center',
-		marginBottom: 70,
-		fontFamily: 'Nunito',
-		textShadowColor: 'white',
-		textShadowRadius: 12
-	},
-	button: {
-		backgroundColor: '#6D2D55',
-		width: 250,
-		height: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 10
-	},
-	modalButton: {
-		backgroundColor: '#6D2D55',
-		width: 200,
-		height: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 10,
-		marginLeft: 'auto',
-		marginRight: 'auto'
-	}
-})
 
 export default DeleteRecipe
