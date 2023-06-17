096fb07d944b5dec7e1bdbeee5a80b840d529e9d
AnthonyMcGreal <anthonymcgreal@hotmail.co.uk>
2023-06-17 21:00:53 +0100

    add useGetRecipe hook

---
+++ app/hooks/useGetRecipe.js
@@ -0,0 +1,31 @@
+import {useState} from 'react'
+import {getRecipee as getRecipeApiCall} from '../api'
+import {useUserContext} from '../components/UserContext'
+import {useAuthContext} from '../components/AuthContext'
+
+const useGetRecipe = () => {
+  const user = useUserContext()
+	const token = useAuthContext()
+
+	const [hasError, setHasError] = useState(false)
+	const [isLoading, setIsLoading] = useState(true)
+	const [recipe, setRecipe] = useState([])
+
+  const getRecipe = async ({recipeName}) => {
+		setHasError(false)
+		setIsLoading(true)
+		const response = await getRecipeApiCall(recipeName, user.user_id, token)
+		if (response.status === 200) {
+			setRecipe(response.data.recipes)
+			setHasError(false)
+		} else {
+			setRecipe([])
+			setHasError(true)
+		}
+		setIsLoading(false)
+	}
+
+	return {hasError, isLoading, recipe, getRecipe}
+}
+
+export default useGetRecipe
